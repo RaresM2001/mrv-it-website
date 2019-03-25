884638300197f472b51f2ba24a2597c7e94e277c
Modure Rares <m.rares956@yahoo.com>
2019-03-25 16:10:39 +0200

    Footer and mockup

--- typed/type-script.js
+++ typed/type-script.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
     var typed = new Typed('.type-animation', {
-        strings: ["design", "code", 'test it.'],
-        typeSpeed: 60,
-        backSpeed: 60,
+        strings: ["design", "code", 'test your app.'],
+        typeSpeed: 50,
+        backSpeed: 50,
         loop: true
     });
 
     var secondTyped = new Typed('.second-type', {
         strings: ["CMSs", "SPAs", 'shopify stores', 'online brands'],
-        typeSpeed: 60,
-        backSpeed: 60,
+        typeSpeed: 55,
+        backSpeed: 55,
         loop: true
     });
 });
