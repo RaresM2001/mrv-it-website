37a34d3f4648bc465b5719c69895d543d0b6e736
Modure Rares <m.rares956@yahoo.com>
2019-03-30 23:36:48 +0200

    Contact page redesign

--- wizzard/wizzard.js
+++ wizzard/wizzard.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 
-    
+    var mailRequestData = {};
 
+    // selecting a project type
+    $('.card').on('click', function() {
+        // toggleing selected class
+        $('.card').removeClass('selected');
+        $(this).addClass('selected');
+        
+        //storging the user input 
+        localStorage.projectType = $(this).attr('data-target');
+        console.log(localStorage.projectType);
+        
+        // redirecting
+        setInterval(function() {
+            window.location.replace('http://127.0.0.1:5500/form.html')
+        }, 500);
+    });
 });
