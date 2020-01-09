6fcf8c538cbf43e491b362c70da00b8f4bf7bf8d
Modure Rares <m.rares956@yahoo.com>
2020-01-09 17:32:24 +0200

    Mockups section first version

--- typed/type-script.js
+++ typed/type-script.js
@@ -1,22 +1,14 @@
+/**
+ * TYPING ANIMATION SCIRPT 
+ * This script simply animates all the typing effects
+ * on this website.
+ */
+
 $(document).ready(function() {
     var typed = new Typed('.type-animation', {
-        strings: ["design", "code", 'test your app.'],
+        strings: ["design", "code", 'deploy your app.'],
         typeSpeed: 50,
         backSpeed: 50,
-        loop: true
-    });
-
-    var secondTyped = new Typed('.second-type', {
-        strings: ["CMSs", "SPAs", 'shopify stores', 'online brands'],
-        typeSpeed: 55,
-        backSpeed: 55,
-        loop: true
-    });
-
-    var thirdTyped = new Typed('.third-type', {
-        strings: ["Let's bring your idea to life"],
-        typeSpeed: 55,
-        backSpeed: 55,
         loop: true
     });
 });
