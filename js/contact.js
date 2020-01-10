c5e618dd597f4d7ac1b595536612fac06cf1ef57
Modure Rares <m.rares956@yahoo.com>
2020-01-10 19:11:24 +0200

    Phone mockups done

---
+++ js/contact.js
@@ -0,0 +1,17 @@
+/**
+ * JS SCRIPT FOR CONTACT PAGE
+ * 
+ * This script handles the contact div animation as well
+ * as the functionality that actually sends the email using 
+ * sendgrid provider.
+ * 
+ * @author Modure Rares
+ */
+
+$(document).ready(function() {
+
+    // this triggers whenever a contact button is pressed
+    $('.contact-btn').on('click', function() {
+        alert('Contact btn pressed!');
+    });
+});
